109cddeba59af30f6b923a492ee85d7b851cbb58
marcoagostinelli <2034731@edu.vaniercollege.qc.ca>
2022-05-04 21:15:37 -0400

    added notifications

--- FinalProject/app/src/main/java/com/example/finalproject/ConfirmBooking.java
+++ FinalProject/app/src/main/java/com/example/finalproject/ConfirmBooking.java
@@ -1,8 +1,13 @@
 package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,9 +27,19 @@ public class ConfirmBooking extends AppCompatActivity {
 
         hotelName.setText(intent.getString("name"));
 
+
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(ConfirmBooking.this, "My Notification");
+                builder.setContentTitle("NOTICE");
+                builder.setContentText("Hotel Successfully Booked");
+                builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(ConfirmBooking.this);
+                managerCompat.notify(1, builder.build());
+
                 startActivity(new Intent(view.getContext(),MainActivity.class));
             }
         });
